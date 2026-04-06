2b9a2dc042c48c9cf160316748b4a813f37e3dd7
zq <zq@example.com>
2026-04-06 20:31:10 +0800

    chore: 添加 Package.swift (Swift 5.7, macOS 12.0+)

---
+++ AliUsageMenuBar/Package.swift
@@ -0,0 +1,12 @@
+// swift-tools-version: 5.7
+import PackageDescription
+
+let package = Package(
+    name: "AliUsageMenuBar",
+    targets: [
+        .executableTarget(
+            name: "AliUsageMenuBar",
+            path: "Sources/AliUsageMenuBar"
+        )
+    ]
+)
