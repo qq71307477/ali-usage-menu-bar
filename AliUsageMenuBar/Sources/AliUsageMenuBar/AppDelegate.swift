163c96b17d8fc60deab851bb1a524cf7e47db53d
zq <zq@example.com>
2026-04-06 21:17:15 +0800

    feat: 菜单栏显示三个时段用量 (5h/周/月)

--- AliUsageMenuBar/Sources/AliUsageMenuBar/AppDelegate.swift
+++ AliUsageMenuBar/Sources/AliUsageMenuBar/AppDelegate.swift
@@ -33,8 +33,8 @@ class AppDelegate: NSObject, NSApplicationDelegate {
 
     func updateStatusItem(fiveHour: Double, week: Double, month: Double) {
         guard let button = statusItem.button else { return }
-        // 显示月度用量百分比
-        button.title = String(format: "%.0f%%", month)
+        // 显示三个时段用量：5h / 周 / 月
+        button.title = String(format: "%.0f%% / %.0f%% / %.0f%%", fiveHour, week, month)
     }
 
     @objc func togglePopover(_ sender: AnyObject?) {
